0a95dcdaa831a3c565e849210a9b0e6c881f06c1
Evg <mishagin02@mail.ru>
2023-06-05 19:26:44 +0300

    add date-line

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,70 +1,77 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Home = () => {
-  const CalendarContainer = styled.div`
-    max-width: 740px;
-    margin: 0 auto;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+const CalendarContainer = styled.div`
+  max-width: 740px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+
+  @media (max-width: 740px) {
     width: 100%;
+  }
+`;
 
-    @media (max-width: 740px) {
-      width: 100%;
-    }
-  `;
+const WeekContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  padding: 10px 0;
+`;
 
-  const WeekContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-    padding: 10px 0;
-  `;
+const Day = styled.div`
+  display: flex;
+  flex-direction: column-reverse; 
+  align-items: center;
+  position: relative;
+`;
 
-  const Day = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    position: relative;
-  `;
 
-  const Line = styled.div`
-    height: 2px;
-    width: 100%;
-    background-color: black;
-  `;
+const Line = styled.div`
+  height: 2px;
+  width: 100%;
+  background-color: black;
+`;
 
-  const Circle = styled.div`
+const Circle = styled.div`
     height: 20px;
     width: 20px;
     border-radius: 50%;
     background-color: red;
     position: absolute;
-    top: 0;
+    top: 55%; /* Изменяем положение кружка */
     left: 50%;
     transform: translateX(-50%);
-  `;
+    order: 1; /* Добавляем порядок отображения */
+  
+`;
 
-  const DayNumber = styled.div`
-    position: relative;
-    z-index: 1;
-    order: 2; /* Изменяем порядок отображения */
+const DayNumber = styled.div`
+  position: relative;
+  z-index: 1;
 
-    /* Стили для белого цвета цифры текущей даты */
-    ${({ isCurrentDay }) => isCurrentDay && `
+
+
+  /* Стили для белого цвета цифры текущей даты */
+  ${({ isCurrentDay }) =>
+    isCurrentDay &&
+    `
       color: white;
     `}
-  `;
+`;
 
-  const DayOfWeek = styled.div`
-    order: 1; /* Изменяем порядок отображения */
-  `;
+const DayOfWeek = styled.div`
+  margin-top: 5px;
+  order: 3; /* Изменяем порядок отображения */
+`;
 
+const Home = () => {
   // Получение текущей даты и дней недели
   const currentDate = new Date();
-  const daysOfWeek = ['M', 'T', 'W', 'T', 'F', 'S', 'S']; // Изменяем порядок дней недели
+  const daysOfWeek = ['M', 'Tu', 'W', 'Th', 'F', 'Sa', 'Su']; // Изменяем порядок дней недели
 
   // Функция для отображения текущей недели
   const renderCurrentWeek = () => {
@@ -77,13 +84,14 @@ const Home = () => {
       const day = new Date(startOfWeek);
       day.setDate(day.getDate() + i);
       const dayOfWeek = daysOfWeek[i];
-      const isCurrentDay = day.getDate() === currentDate.getDate() && day.getMonth() === currentDate.getMonth();
+      const isCurrentDay =
+        day.getDate() === currentDate.getDate() && day.getMonth() === currentDate.getMonth();
 
       weekDays.push(
         <Day key={dayOfWeek}>
-          {isCurrentDay && <Circle />}
           <DayOfWeek>{dayOfWeek}</DayOfWeek>
           <DayNumber isCurrentDay={isCurrentDay}>{day.getDate()}</DayNumber>
+             {isCurrentDay && <Circle />}
         </Day>
       );
     }
@@ -96,6 +104,7 @@ const Home = () => {
       <h1>Interview calendar</h1>
       <Line />
       <WeekContainer>{renderCurrentWeek()}</WeekContainer>
+      <Line />
     </CalendarContainer>
   );
 };
