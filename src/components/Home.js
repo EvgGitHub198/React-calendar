e7d4a44cab41e4713025e26a8d7e150bc14e51d1
Evg <mishagin02@mail.ru>
2023-06-06 21:57:50 +0300

    add nav

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const CalendarContainer = styled.div`
@@ -16,19 +16,18 @@ const CalendarContainer = styled.div`
 
 const WeekContainer = styled.div`
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
   align-items: center;
-  width: 100%;
+  width: 90%;
   padding: 10px 0;
 `;
 
 const Day = styled.div`
   display: flex;
-  flex-direction: column-reverse; 
+  flex-direction: column-reverse;
   align-items: center;
   position: relative;
 `;
-
 
 const Line = styled.div`
   height: 2px;
@@ -37,25 +36,21 @@ const Line = styled.div`
 `;
 
 const Circle = styled.div`
-    height: 20px;
-    width: 20px;
-    border-radius: 50%;
-    background-color: red;
-    position: absolute;
-    top: 55%; /* Изменяем положение кружка */
-    left: 50%;
-    transform: translateX(-50%);
-    order: 1; /* Добавляем порядок отображения */
-  
+  height: 22px;
+  width: 22px;
+  border-radius: 55%;
+  background-color: red;
+  position: absolute;
+  top: 55%;
+  left: 50%;
+  transform: translateX(-50%);
+  order: 1;
 `;
 
 const DayNumber = styled.div`
   position: relative;
   z-index: 1;
 
-
-
-  /* Стили для белого цвета цифры текущей даты */
   ${({ isCurrentDay }) =>
     isCurrentDay &&
     `
@@ -65,19 +60,48 @@ const DayNumber = styled.div`
 
 const DayOfWeek = styled.div`
   margin-top: 5px;
-  order: 3; /* Изменяем порядок отображения */
+  order: 3;
+`;
+
+const MonthYear = styled.div`
+  margin: 10px 0;
+  text-align: center;
+  display: flex;
+  justify-content: space-between; /* Align arrows at the edges */
+  align-items: center;
+  width: 100%; /* Add width to occupy entire space */
+  padding: 0 10px; /* Add padding to create space around arrows */
+`;
+
+const Arrow = styled.div`
+  cursor: pointer;
+  color: red;
+  font-weight: bold;
+
+  margin: 10px 5%;
 `;
 
 const Home = () => {
-  // Получение текущей даты и дней недели
-  const currentDate = new Date();
-  const daysOfWeek = ['M', 'Tu', 'W', 'Th', 'F', 'Sa', 'Su']; // Изменяем порядок дней недели
+  const [currentDate, setCurrentDate] = useState(new Date());
 
-  // Функция для отображения текущей недели
+  const daysOfWeek = ['M', 'Tu', 'W', 'Th', 'F', 'Sa', 'Su'];
+
+  const handlePrevWeek = () => {
+    const newDate = new Date(currentDate);
+    newDate.setDate(newDate.getDate() - 7);
+    setCurrentDate(newDate);
+  };
+
+  const handleNextWeek = () => {
+    const newDate = new Date(currentDate);
+    newDate.setDate(newDate.getDate() + 7);
+    setCurrentDate(newDate);
+  };
+
   const renderCurrentWeek = () => {
-    const currentDayIndex = (currentDate.getDay() + 6) % 7; // Исправляем индекс текущего дня недели
-    const startOfWeek = new Date(currentDate); // Начало текущей недели
-    startOfWeek.setDate(startOfWeek.getDate() - currentDayIndex); // Установка начальной даты недели
+    const currentDayIndex = (currentDate.getDay() + 6) % 7;
+    const startOfWeek = new Date(currentDate);
+    startOfWeek.setDate(startOfWeek.getDate() - currentDayIndex);
 
     const weekDays = [];
     for (let i = 0; i < 7; i++) {
@@ -91,7 +115,7 @@ const Home = () => {
         <Day key={dayOfWeek}>
           <DayOfWeek>{dayOfWeek}</DayOfWeek>
           <DayNumber isCurrentDay={isCurrentDay}>{day.getDate()}</DayNumber>
-             {isCurrentDay && <Circle />}
+          {isCurrentDay && <Circle />}
         </Day>
       );
     }
@@ -101,9 +125,16 @@ const Home = () => {
 
   return (
     <CalendarContainer>
-      <h1>Interview calendar</h1>
+      <h1 className="header">Interview calendar</h1>
       <Line />
       <WeekContainer>{renderCurrentWeek()}</WeekContainer>
+      <MonthYear>
+        <Arrow onClick={handlePrevWeek}>&lt;</Arrow>
+        {new Intl.DateTimeFormat('en-US', { month: 'long', year: 'numeric' }).format(
+          currentDate
+        )}
+        <Arrow onClick={handleNextWeek}>&gt;</Arrow>
+      </MonthYear>
       <Line />
     </CalendarContainer>
   );
